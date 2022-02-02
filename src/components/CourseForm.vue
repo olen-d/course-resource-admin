2512ab63c085c9192cb250c1f75ea35a6ccb7e18
olen-d <olen.daelhousen@outlook.com>
2022-02-02 00:11:58 -0500

    Create title input

--- src/components/CourseForm.vue
+++ src/components/CourseForm.vue
@@ -3,25 +3,29 @@
     <n-alert v-if="showErrorMessageBox"
       :title="errorTitle"
       type="error"
-      :description="errorDescription"
       :show-icon="true"
       style="margin-bottom: 1.5rem; text-align: left"
     >
+      {{errorDescription}}
     </n-alert>
     <n-form label-position="top">
     </n-form>
+    <InputTitle @changeTitle="updateFormValues($event)" />
     <n-button @click="handleSubmit" type="primary" attr-type="submit">{{submitActionLabel}}</n-button>
+    {{formValues.title}}
     <br />
   </div>
 </template>
 
 <script>
+import InputTitle from './form-fields/InputTitle.vue'
+
 import { defineComponent, ref } from 'vue'
 
 import { NAlert, NButton, NForm } from 'naive-ui'
 
 export default defineComponent({
-  components: { NAlert, NButton, NForm },
+  components: { InputTitle, NAlert, NButton, NForm },
   props: {
     submitActionLabel: {
       type: String,
@@ -29,20 +33,62 @@ export default defineComponent({
     }
   },
   setup () {
+    const errorDescription = ref('')
+    const errorTitle = ref('')
+    const formValues = ref([])
     const showErrorMessageBox = ref(false)
+
     const handleSubmit = async () => {
-      // Check for errors
-      // Show error box if fields did not pass validation
+      const formErrors = getFormErrors()
+
+      if (formErrors.length > 0) {
+        const errorMessages = formErrors.map(element => {
+          return element.errorMessage
+        })
+        errorDescription.value = errorMessages.join('. ') + '.'
+
+        const numberAgreement = formErrors.length === 1 ? 'An Error' : 'Errors'
+        errorTitle.value = `The New Course Form Has ${numberAgreement}`
+
+        showErrorMessageBox.value = true
+      } else {
+        showErrorMessageBox.value = false
       // Otherwise submit
+      }
+    }
+
+    const getFormErrors = () => {
+      const formErrors = formValues.value.filter(element => {
+        return element.isValid === false
+      })
+      return formErrors
+      // const errorsIndex = formValues.value.findIndex(element => element.isValid === false)
+      // return errorsIndex !== -1
+    }
+
+    const updateFormValues = event => {
+      const { inputName: name } = event
+      const valuesIndex = formValues.value.findIndex(element => element.inputName === name)
+      if (valuesIndex === -1) {
+        formValues.value.push(event)
+      } else {
+        formValues.value[valuesIndex] = event
+      }
+      // if (showErrorMessageBox.value) { validateForm() }
+      // TODO: Check formErrors for true, if found, disable the submit button, otherwise enable the submit button
     }
     return {
+      errorDescription,
+      errorTitle,
+      formValues,
       handleSubmit,
-      showErrorMessageBox
+      showErrorMessageBox,
+      updateFormValues
     }
   }
 })
 // Inputs:
-// Title
+// x Title
 // Slug
 // isPublished
 // publishOn
