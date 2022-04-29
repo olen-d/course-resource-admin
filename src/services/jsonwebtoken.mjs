33eeb759304cd00cb7bbc53430a7d5c869510585
olen-d <olen.daelhousen@outlook.com>
2022-04-28 23:03:14 -0400

    Fix verify JWT to throw an error

--- src/services/jsonwebtoken.mjs
+++ src/services/jsonwebtoken.mjs
@@ -4,9 +4,13 @@ const verifyBearerToken = async (accessToken, publicKey) => {
   const jwtAlgorithm = import.meta.env.VITE_JWT_ALGORITHM
   const jwtIssuer = import.meta.env.VITE_JWT_ISSUER
 
-  const ecPublicKey = await jose.importSPKI(publicKey, jwtAlgorithm)
-  const result = await jose.jwtVerify(accessToken, ecPublicKey, { issuer: jwtIssuer })
-  return result
+  try {
+    const ecPublicKey = await jose.importSPKI(publicKey, jwtAlgorithm)
+    const result = await jose.jwtVerify(accessToken, ecPublicKey, { issuer: jwtIssuer })
+    return result
+  } catch (error) {
+    throw new Error(error)
+  }
 }
 
 export { verifyBearerToken }
