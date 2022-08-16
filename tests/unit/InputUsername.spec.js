135dd73022d10e4fc4ad8306cbdc911ca1dbe33a
olen-d <olen.daelhousen@outlook.com>
2022-08-16 00:22:30 -0400

    Test Inputusername Updates v-model

--- tests/unit/InputUsername.spec.js
+++ tests/unit/InputUsername.spec.js
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest'
-import { render, screen } from '@testing-library/vue'
+import { cleanup, fireEvent, render, screen } from '@testing-library/vue'
 import InputUsername from '@/components/form-fields/InputUsername.vue'
 
 test('default props render', async () => {
@@ -7,6 +7,8 @@ test('default props render', async () => {
 
   expect(screen.getByLabelText('Username *')).toBeTruthy()
   expect(screen.getByPlaceholderText('Enter your username...')).toBeTruthy()
+
+  cleanup()
 })
 
 test('passed props render', async () => {
@@ -14,4 +16,15 @@ test('passed props render', async () => {
 
   expect(screen.getByLabelText('Email Address *')).toBeTruthy()
   expect(screen.getByPlaceholderText('Enter your email address...')).toBeTruthy()
+
+  cleanup()
+})
+
+test('input updates v-model', async () => {
+  render(InputUsername)
+
+  const input = screen.getByLabelText('Username *')
+  await fireEvent.update(input, 'Fred')
+
+  expect(input._value).toEqual('Fred')
 })
