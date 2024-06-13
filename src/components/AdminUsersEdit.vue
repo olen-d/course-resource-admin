70f275f6497ef31ec00b9e353a7954ba3dfdf1d5
Olen Daelhousen <olen@roman.search.charter.net>
2024-06-12 23:53:49 -0400

    Add feature to administer user profiles

---
+++ src/components/AdminUsersEdit.vue
@@ -0,0 +1,22 @@
+<script setup>
+import FormUserUpsert from '@/components/FormUserUpsert.vue'
+
+const props = defineProps({
+  userId: {
+      type: String,
+      required: true
+    }
+})
+</script>
+
+<template>
+  <div class="admin-users-edit">
+    <h3>Edit User</h3>
+    <FormUserUpsert
+      form-action="edit"
+      form-name="Edit User"
+      submit-action-label="Update User"
+      success-action="updated"
+      :user-id="userId"/>
+  </div>
+</template>
